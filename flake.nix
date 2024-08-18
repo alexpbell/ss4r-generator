d4adbb9370af2660eecbf5f950965e310ab16666
alexpbell <alexblaircampbell@icloud.com>
2024-08-18 15:12:38 +1000

    revert to flake-in-situ

--- flake.nix
+++ flake.nix
@@ -354,8 +354,10 @@
           installPhase = ''
             mkdir -p $out/r-package
             cp -r ss4r $out/r-package
-            cp ss4r-files/DESCRIPTION $out/r-package/ss4r
-            cp ss4r-files/R/ss4r-package.R $out/r-package/ss4r/R
+            printf 'Package: ss4r\nTitle: Stock Synthesis 4 R\nVersion: 0.0.0.9000\nAuthors@R:\n person("Campbell", "Alexander", , "alex@alexpbell.com", role = c("aut", "cre"), comment = c(ORCID = "YOUR-ORCID-ID"))\nDescription: Stock Synthesis as a "pure" R package\nLicense: CC0 + file LICENSE\nEncoding: UTF-8\nRoxygen: list(markdown = TRUE)\nRoxygenNote: 7.3.1\nLinkingTo:\n    Rcpp\nImports:    Rcpp' > DESCRIPTION
+            cp DESCRIPTION $out/r-package/ss4r
+            printf "#' Call Stock Synthesis\n#' @param x anything right now\n#' @export\ncall_stock_synthesis <- function(x) { call_ss3_notmain(x) }" > ss4r-package.R
+            cp ss4r-package.R $out/r-package/ss4r/R
           '';
         };
 
