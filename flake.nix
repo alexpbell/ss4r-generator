8318fee80551148a7c90caec2110a298b5a849eb
Alexander Campbell <ghostofsandy@gmail.com>
2024-08-19 20:38:08 +1000

    filset to pull DESCRIPTION from local folder ss4r-mocking into build

--- flake.nix
+++ flake.nix
@@ -37,6 +37,7 @@
               owner = "nmfs-ost";
               repo = "ss3-test-models";
               rev = "ad02c34";
+              name = "data";
               sha256 = "2nqEzzKQROlsmS9SLZ+H3Fv/QDWKUeedVZdX+1w8eqw=";
             })
             (pkgs.lib.fileset.toSource {
@@ -354,9 +355,7 @@
             rm ss3.cpp
             mv ss3_tmp.cpp ss3.cpp
             sed -i '1s/^/#include <Rcpp.h>\n/' ss3.cpp
-            Rscript -e 'Rcpp::Rcpp.package.skeleton("ss4r", path=".", cpp_files=Sys.glob(c("*.cpp","*.h")), example_code=FALSE, attributes=TRUE)'           
-            printf 'Package: ss4r\nTitle: Stock Synthesis 4 R\nVersion: 0.0.0.9000\nAuthors@R:\n person("Campbell", "Alexander", , "alex@alexpbell.com", role = c("aut", "cre"), comment = c(ORCID = "YOUR-ORCID-ID"))\nDescription: Stock Synthesis as a "pure" R package\nLicense: CC0 + file LICENSE\nEncoding: UTF-8\nRoxygen: list(markdown = TRUE)\nRoxygenNote: 7.3.1\nLinkingTo:\n    Rcpp\nImports:\n    Rcpp\n' > DESCRIPTION
-            cp DESCRIPTION ss4r
+            Rscript -e 'Rcpp::Rcpp.package.skeleton("ss4r", path=".", cpp_files=Sys.glob(c("*.cpp","*.h")), example_code=FALSE, attributes=TRUE)'           	   cp source/DESCRIPTION ss4r
             printf "#' Call Stock Synthesis\n#' @param x anything right now!\n#' @export\ncall_stock_synthesis <- function(x) { call_ss3_notmain(x) }" > ss4r-package.R
             cp ss4r-package.R ss4r/R
 	    Rscript -e 'roxygen2::roxygenise("ss4r")'
