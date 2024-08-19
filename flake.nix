9174a28e9df1557ffb8b42375ce4fe030178cb70
Alexander Campbell <ghostofsandy@gmail.com>
2024-08-19 18:55:39 +1000

    trial roxygenise again

--- flake.nix
+++ flake.nix
@@ -352,7 +352,8 @@
             printf 'Package: ss4r\nTitle: Stock Synthesis 4 R\nVersion: 0.0.0.9000\nAuthors@R:\n person("Campbell", "Alexander", , "alex@alexpbell.com", role = c("aut", "cre"), comment = c(ORCID = "YOUR-ORCID-ID"))\nDescription: Stock Synthesis as a "pure" R package\nLicense: CC0 + file LICENSE\nEncoding: UTF-8\nRoxygen: list(markdown = TRUE)\nRoxygenNote: 7.3.1\nLinkingTo:\n    Rcpp\nImports:\n    Rcpp\n' > DESCRIPTION
             cp DESCRIPTION ss4r
             printf "#' Call Stock Synthesis\n#' @param x anything right now\n#' @export\ncall_stock_synthesis <- function(x) { call_ss3_notmain(x) }" > ss4r-package.R
-            cp ss4r-package.R ss4r/R            
+            cp ss4r-package.R ss4r/R
+	    Rscript -e 'roxygen2::roxygenise("ss4r")' 
           '';
 
           installPhase = ''
